b59f30eaf260e415ba74e8017a9dcf9452404262
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-08-28 13:08:22 -0500

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -7,6 +7,9 @@ from random import random
 import random
 import matplotlib.animation as animation
 
+coords=[]
+angles = []
+maxPoints = 100
 
 def x(a):
   return 1*math.cos(a)
@@ -18,22 +21,18 @@ def showGraph(arr):
 	xs, ys = zip(*arr) 
 	plt.figure()
 	plt.plot(xs,ys, 'black') 
-	plt.xlim(-2, 2)
-	plt.ylim(-2, 2)
+	plt.xlim(-1.5, 1.5)
+	plt.ylim(-1.5, 1.5)
 	plt.gca().set_aspect('equal', adjustable='box')
-	#get current axes
 	ax = plt.gca()
-	#hide x-axis
 	ax.get_xaxis().set_visible(False)
-	#hide y-axis 
 	ax.get_yaxis().set_visible(False)
 
 def mainFunction(coords_local, angles_local):
-	seed(1)
-	for _ in range(100):
+	seed()
+	for _ in range(maxPoints):
 		value = random.randrange(6200)/1000
 		angles_local.append(value)
-	print(angles_local)
 	angles_local.sort()
 	
 	for a in angles_local: 
@@ -42,23 +41,35 @@ def mainFunction(coords_local, angles_local):
 	#showGraph(coords_local)
 
 def mutate(coords_local):
-	seed(1)
-	for _ in range(180):
-		value = random.randint(1,62)  #select random points to modify them
-		coords_local[value][0]=coords_local[value][0]+random.randint(1,10)/10000
-		coords_local[value][0]=coords_local[value][0]-random.randint(1,10)/10000
+	print(angles)
+	seed()
+	for _ in range(len(angles)):
+		value = random.randint(1,maxPoints)  #select random points to modify them
+		z= random.randint(-1,1)/100
+		coords_local[value][0]=coords_local[value][0]+ z
+		z= random.randint(-1,1)/100
+		coords_local[value][1]=coords_local[value][1]+ z
 
-coords=[]
-angles = []
+		if (value>0):
+			coords_local[value-1][0]=coords_local[value-1][0]+ z/2
+			coords_local[value-1][1]=coords_local[value-1][1]+ z/2
+		else:
+			coords_local[len(coords_local)][0]=coords_local[value-1][0]+ z/2
+			coords_local[len(coords_local)][1]=coords_local[value-1][1]+ z/2
+
+
+		if (value<maxPoints):
+			coords_local[value+1][0]=coords_local[value+1][0]+ z/2
+			coords_local[value+1][1]=coords_local[value+1][1]+ z/2
+		else:
+			coords_local[0][0]=coords_local[value-1][0]+ z/2
+			coords_local[0][1]=coords_local[value-1][1]+ z/2
+	showGraph(coords)
+
 mainFunction(coords, angles)
 
-print("start mutating")
-
-for _ in range(100):
-	seed(4)
+for _ in range(10):
 	mutate(coords)
 
-showGraph(coords)
 
 plt.show()
-print("end")
