c4c8cf7cb9bd30f008c551eea6623fbd3d7f8159
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-08-28 12:00:15 -0500

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -18,8 +18,8 @@ def showGraph(arr):
 	xs, ys = zip(*arr) 
 	plt.figure()
 	plt.plot(xs,ys, 'black') 
-	plt.xlim(-1, 1)
-	plt.ylim(-1, 1)
+	plt.xlim(-2, 2)
+	plt.ylim(-2, 2)
 	plt.gca().set_aspect('equal', adjustable='box')
 	#get current axes
 	ax = plt.gca()
@@ -27,24 +27,38 @@ def showGraph(arr):
 	ax.get_xaxis().set_visible(False)
 	#hide y-axis 
 	ax.get_yaxis().set_visible(False)
-	plt.show()
 
-def mainFunction():
-	coords=[]
-	angles = []
-	##### list of all angles we want to use
+def mainFunction(coords_local, angles_local):
 	seed(1)
 	for _ in range(100):
-		value = random.randrange(62)/10
-		angles.append(value)
-	#angles.sort()
+		value = random.randrange(6200)/1000
+		angles_local.append(value)
+	print(angles_local)
+	angles_local.sort()
 	
-	for a in angles: 
-		coords.append([x(a),y(a)])
-		coords.append(coords[0])
-	showGraph(coords)
+	for a in angles_local: 
+		coords_local.append([x(a),y(a)])
+	coords_local.append(coords_local[0])
+	#showGraph(coords_local)
 
-mainFunction()
-   
+def mutate(coords_local):
+	seed(1)
+	for _ in range(180):
+		value = random.randint(1,62)  #select random points to modify them
+		coords_local[value][0]=coords_local[value][0]+random.randint(1,10)/10000
+		coords_local[value][0]=coords_local[value][0]-random.randint(1,10)/10000
 
+coords=[]
+angles = []
+mainFunction(coords, angles)
 
+print("start mutating")
+
+for _ in range(100):
+	seed(4)
+	mutate(coords)
+
+showGraph(coords)
+
+plt.show()
+print("end")
