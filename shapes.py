95dac0c82be32b6bed92e34b9cf1ff04e0fcd0c7
victorlmx <112336409+victorlmx@users.noreply.github.com>
2022-08-28 19:57:12 -0500

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -8,10 +8,13 @@ import random
 import matplotlib.animation as animation
 
 
-maxPrints = 10
+maxPrints = 50
+mutations = 0
+coords=[]
 
-def showGraph(arr):
-	xs, ys = zip(*arr) 
+def showGraph(show=0):
+	global coords
+	xs, ys = zip(*coords) 
 	plt.figure()
 	plt.plot(xs,ys, 'black') 
 	plt.xlim(-1.5, 1.5)
@@ -20,16 +23,19 @@ def showGraph(arr):
 	ax = plt.gca()
 	ax.get_xaxis().set_visible(False)
 	ax.get_yaxis().set_visible(False)
-	print("Printing plot..")
-	plt.show()
 	global maxPrints
-	maxPrints -= 1
+	if(show==1):
+		plt.show()
+		maxPrints -= 1
 	if (maxPrints<0):
-		print("max number of prints, exiting..")
+		#printDebug("###max number of prints, exiting..")
 		exit()
 
 
-def initialize(coords_local):
+
+
+def initialize():
+	global coords
 	def x(a):
 		return 1*math.cos(a)
 
@@ -38,92 +44,104 @@ def initialize(coords_local):
 
 	seed()
 	angles = []
-	for i in range(math.floor(round(2*math.pi,3)*100)): #change the line if you want more points in the circle
+	for i in range(math.floor(round(2*math.pi,3)*100)): #change the line if you want more points in the circles
 		angles.append(i/100)
 
 		
-	print("Your circle has these many points",len(angles))
-	
+	print("###Your circle has these many points",len(angles))
+	#printDebug(angles)
 	for a in angles: 
-		coords_local.append([x(a),y(a)]) #get the coordinates for each angle
+		coords.append([x(a),y(a)]) #get the coordinates for each angle
 
-	coords_local.append(coords_local[0]) #close the circle
-	showGraph(coords) #paint the circle prior to changing it
+	coords.append(coords[0]) #close the circle
 
 
-def mutate(coords_local):
+def mutate():
+	global coords
 	seed()
-	for v in range(len(coords_local)):
+	for v in range(len(coords)):
 		def mutate_run():
-			print("Running mutate for v: ", v)
-			zx= random.randint(-1,1)/10
-			zy= random.randint(-1,1)/10
-			print("zx ", zx, "zy ", zy)
-			print("Original values:")
-			print(coords_local[v])
-			coords_local[v][0]=coords_local[v][0]+ zx
-			coords_local[v][1]=coords_local[v][1]+ zy
-			print("Changed values:")
-			print(coords_local[v])
+			global mutations
+			mutations += 1
+			#print("##mutation: ", mutations, " v: ", v)
+			#print("###Running mutate for v: ", v)
+			zx= random.randint(-50,50)/1000
+			zy= random.randint(-50,50)/1000
+			#print(coords[v])
+			coords[v][0]=coords[v][0]+ zx
+			coords[v][1]=coords[v][1]+ zy
+			#print("zx and zy",zx,zy)
+			#print(coords[v])
+			#showGraph(1)
 
-			def roundDown(vv,i):
-				print("runDown..", vv)
-				print("zx ", zx, "zy ", zy)
-				if (vv-1>0 and vv-1<629):
-					print("Original values:")
-					print(coords_local[vv-1])
-					coords_local[vv-1][0]=coords_local[vv-1][0]+ zx/i
-					coords_local[vv-1][1]=coords_local[vv-1][1]+ zy/i
-					print("After:")
-					print(coords_local[vv-1])
+			def roundDown(vv,ranx, rany):
+				#print("###runDown.. v:", vv, "ranx and rany:", ranx, rany)
+				if (vv>0 and vv<629):
+					ogx=coords[vv][0]
+					ogy=coords[vv][1]
+					coords[vv][0]=(ogx+(ogx+ ranx))/2
+					coords[vv][1]=(ogy+(ogy+ rany))/2
 				else:
-					print("Original values:")
-					print(coords_local[len(coords_local)-1])
-					coords_local[len(coords_local)-1][0]=coords_local[len(coords_local)-1][0]+ zx/i
-					coords_local[len(coords_local)-1][1]=coords_local[len(coords_local)-1][1]+ zy/i
-					print("After:")
-					print(coords_local[len(coords_local)-1])
-				showGraph(coords)
+					ogx=coords[len(coords)-1][0]
+					ogy=coords[len(coords)-1][1]
+					coords[len(coords)-1][0]=(ogx+(ogx+ ranx))/2
+					coords[len(coords)-1][1]=(ogy+(ogy+ rany))/2
+				#showGraph(1)
 
-			def roundUp(vv,i):
-				print("runUp..", vv)
-				print("zx ", zx, "zy ", zy)
-				if (vv+1<len(coords_local)-1):
-					print("Original values:")
-					print(coords_local[vv+1])
-					coords_local[vv+1][0]=coords_local[vv+1][0]+ zx/i
-					coords_local[vv+1][1]=coords_local[vv+1][1]+ zy/i
-					print("After:")
-					print(coords_local[vv+1])
+			def roundUp(vv,ranx,rany):
+				#print("###runUp.. v:", vv, "ranx and rany:", ranx, rany)
+				#printDebug("zx/i ", zx/i, "zy/i ", zy/i)
+				if (vv<len(coords)-1):
+					#printDebug("Original values:")
+					#printDebug(coords[vv])
+					ogx=coords[vv][0]
+					ogy=coords[vv][1]
+					coords[vv][0]=(ogx+(ogx+ ranx))/2
+					coords[vv][1]=(ogy+(ogy+ rany))/2
 				else:
-					print("Original values:")
-					print(coords_local[0])
-					coords_local[0][0]=coords_local[0][0]+ zx/i
-					coords_local[0][1]=coords_local[0][1]+ zy/i
-					print("After:")
-					print(coords_local[0])
-				showGraph(coords)
+					ogx=coords[0][0]
+					ogy=coords[0][1]
+					coords[0][0]=(ogx+(ogx+ ranx))/2
+					coords[0][1]=(ogy+(ogy+ rany))/2
+				#showGraph(1)
 
-			roundUp(v+1,2)
-			roundUp(v+2,4)
-			roundUp(v+3,8)
-			roundDown(v-1,2)
-			roundDown(v-2,4)
-			roundDown(v-3,8)
+			if(random.randint(0,60)<50):  #decide whether to edit the point or not
+				roundUp(v+1,zx,zy)
+				roundUp(v+2,zx,zy)
+				roundUp(v+3,zx/1.5,zy/1.5)
+				roundUp(v+4,zx/1.5,zy/1.5)
+				roundDown(v-1,zx,zy)
+				roundDown(v-2,zx,zy)
+				roundDown(v-3,zx/1.5,zy/1.5)
+				roundDown(v-4,zx/1.5,zy/1.5)
+				#print("rounded successfully v:", v)
+			#else:
+				#print("NO ROUNDDOWNS AND UPS? v", v)
 
-		print("Check if v will mutate.. : ", v)
-		if(random.randint(0,15)==0):  #decide whether to edit the point or not
+		if (v > len(coords)-1):
+			break
+		if(random.randint(0,60)==0):  #decide whether to edit the point or not
 			mutate_run()
+		else:
+			if(random.randint(0,25)==0): 
+				#print("popping v", v)
+				coords.pop(v)
 
 
-coords=[]
-edits=1
 
-initialize(coords)
+
+edits=50
+
+initialize()
 
 for _ in range(edits):
-	mutate(coords)
-	showGraph(coords)
+	#print("mutation")
+	mutate()
+	coords.append(coords[0]) #close the circle
+	showGraph(1)
 
+coords.append(coords[0]) #close the circle
+print("###Your circle only has these many points now",len(coords))
+showGraph(1)
 
 
